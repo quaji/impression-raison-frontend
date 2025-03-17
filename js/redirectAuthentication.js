b2ef7ae958e3d28c84e7801b6f153ad3c1bc2d6f
Quaj <lychee6to7@gmail.com>
2025-03-18 03:09:36 +0900

    何が起きてる？

--- js/redirectAuthentication.js
+++ js/redirectAuthentication.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     axios.get("https://impression-raison-backendapps-e7ejecbkdeggergd.japaneast-01.azurewebsites.net/sign/auth")
     .then(response => {
+        console.log("response",response);
         if(!response){
             window.Location.href = "https://lemon-water-022469c10.6.azurestaticapps.net/authentication";
         }
