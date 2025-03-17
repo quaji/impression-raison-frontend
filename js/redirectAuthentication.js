6fa4d8516c4c03106cda8ee1b1b23e9183064d84
Quaj <lychee6to7@gmail.com>
2025-03-18 03:17:40 +0900

    errer変更

--- js/redirectAuthentication.js
+++ js/redirectAuthentication.js
@@ -2,7 +2,7 @@ window.onload = function(){
     axios.get("https://impression-raison-backendapps-e7ejecbkdeggergd.japaneast-01.azurewebsites.net/sign/auth")
     .then(response => {
         console.log("response",response);
-        if(!response){
+        if(!response["uid"]){
             window.Location.href = "https://lemon-water-022469c10.6.azurestaticapps.net/authentication";
         }
     })
