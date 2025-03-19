ab21a08722440dd7c9549f145c4140dfce61105b
Quaj <lychee6to7@gmail.com>
2025-03-20 04:23:46 +0900

    temp

--- js/redirectAuthentication.js
+++ js/redirectAuthentication.js
@@ -2,13 +2,13 @@ window.onload = function(){
     axios.get("https://impression-raison-backendapps-e7ejecbkdeggergd.japaneast-01.azurewebsites.net/sign/auth")
     .then(response => {
         const uid = parseInt(response.data.uid);
+        const link = response.data.link;
         console.log("response",uid);
         if(!uid){
-            window.location.href = "https://lemon-water-022469c10.6.azurestaticapps.net/authentication";
+            window.location.href = link;
         }
     })
     .catch(error=>{
         console.error("ERROR:",error);
-        window.location.href = "https://lemon-water-022469c10.6.azurestaticapps.net/authentication";
     });
 };
