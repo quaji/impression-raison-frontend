f7ce6e7bce0db8611c96a8a0f623fbff7c80bc9e
Quaj <lychee6to7@gmail.com>
2025-03-20 03:25:27 +0900

    a

--- js/redirectAuthentication.js
+++ js/redirectAuthentication.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     axios.get("https://impression-raison-backendapps-e7ejecbkdeggergd.japaneast-01.azurewebsites.net/sign/auth")
     .then(response => {
-        const uid = response.data.uid;
+        const uid = parseInt(response.data.uid);
         console.log("response",uid);
         if(uid == 0){
             window.Location.href = "https://lemon-water-022469c10.6.azurestaticapps.net/authentication";
