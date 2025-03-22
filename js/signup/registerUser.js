c72637ad021182d29c7e0b36456cd8a02d6f82e1
Quaj <lychee6to7@gmail.com>
2025-03-23 04:37:33 +0900

    sign up section ver1

---
+++ js/signup/registerUser.js
@@ -0,0 +1,42 @@
+const btn = document.getElementById("submitButton");
+const tempCode = document.getElementById("tempCode");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
+const username = document.getElementById("username");
+
+btn.addEventListener("click",()=>{
+    if(!tempCode.value)document.getElementById("cautionTC").hidden =false;
+    if(!password.value)document.getElementById("cautionP").hidden =false;
+    if(!passwordConfirm.value)document.getElementById("cautionPC").hidden =false;
+    if(tempCode.value && password.value && passwordConfirm.value){
+        if(password.value!=passwordConfirm.value)window.alert("パスワードと確認パスワードが一致していませんでした。");
+        else{
+            if (btnContainer.classList.contains("disabled")) return;
+            btnContainer.classList.add("disabled");
+            axios.post("https://impression-raison-backendapps-e7ejecbkdeggergd.japaneast-01.azurewebsites.net/sign/up",
+                {
+                    "tempCode":tempCode.value,
+                    "password":password.value,
+                    "username":username.value
+                }
+            )
+            .then(response => {
+                if(response.data.link){
+                    const  link = response.data.link;
+                    if(response.data.message){
+                        window.alert(response.data.message);
+                    }
+                    window.location.href = link;
+                }else if(response.data.message){
+                    window.alert(response.data.message);
+                }
+            })
+            .catch(error =>{
+                console.error(error);
+            })
+            .finally(() => {
+                btn.classList.remove("disabled");
+            });
+        }
+    }
+});
